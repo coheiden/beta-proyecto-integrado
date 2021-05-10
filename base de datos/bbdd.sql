0304888875544c162e806bb0d81866df8830513c
pasacl <pauglu777@gmail.com>
2021-05-10 09:38:31 +0200

    Base de datos//Finalizada

---
+++ base de datos/bbdd.sql
@@ -0,0 +1,46 @@
+DROP DATABASE IF EXISTS eventos;
+CREATE DATABASE eventos;
+USE eventos;
+
+CREATE TABLE IF NOT EXISTS administrador(
+    id_administrador INT PRIMARY KEY, 
+    nombre_usuario VARCHAR(20),
+    contraseña VARCHAR(20),
+    correo VARCHAR(20)
+);
+
+CREATE TABLE IF NOT EXISTS asistente(
+    id_asistente INT PRIMARY KEY,
+    dni VARCHAR(9),
+    fecha_hora_registro DATETIME,
+    codigo_postal INT(5),
+    nombre VARCHAR(20),
+    apellidos VARCHAR(20),
+    correo VARCHAR(20)
+);
+
+CREATE TABLE IF NOT EXISTS acto(
+    id_acto INT PRIMARY KEY,
+    id_administrador INT,  
+    nombre VARCHAR(20),
+    descripcion VARCHAR(20),
+    direccion VARCHAR(20),
+    fecha_hora DATETIME,
+    plazas_ocupadas INT,
+    plazas_totales INT,
+
+    CONSTRAINT fk_id_administrador FOREIGN KEY (id_administrador)
+    REFERENCES administrador (id_administrador)
+);
+
+CREATE TABLE asistente_acto(
+    id INT PRIMARY KEY,
+    id_acto INT,
+    id_asistente INT,
+
+    CONSTRAINT fk_id_acto FOREIGN KEY (id_acto)
+    REFERENCES acto (id_acto),
+
+    CONSTRAINT fk_id_cliente FOREIGN KEY (id_asistente)
+    REFERENCES asistente (id_asistente)
+)
