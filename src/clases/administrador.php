e95a254e2d3e425ce5f2c12762872e5a9f961a3d
pasacl <pauglu777@gmail.com>
2021-05-12 13:05:47 +0200

    Fixed Admin

--- src/clases/administrador.php
+++ src/clases/administrador.php
@@ -6,16 +6,6 @@ class administrador extends conexion
     public function obtenerInfo($usuario)
     {
 
-<<<<<<< HEAD:src/clases/administrador.php
-    public function obtenerInfo($usuario){
-
-        $sql = "SELECT * from administrador where nombre_usuario = '$usuario'";
-        $result = $this->connect()->query($sql);
-
-        $array_info = $result->fetch_all(MYSQLI_ASSOC);
-
-        return $array_info;
-=======
         $sql = 'SELECT * from administrador where nombre_usuario = "' . $usuario . '"';
         //echo $sql;
         $result = $this->connect()->query($sql);
@@ -23,7 +13,6 @@ class administrador extends conexion
             $infoUser = $result->fetch_assoc();
             return $infoUser;
         };
->>>>>>> 9e6908283ed2f3190b1dd5c3415492948333ba94:clases/administrador.php
 
     }
 
@@ -32,30 +21,6 @@ class administrador extends conexion
         $user = $datosForm["user"];
         $password = $datosForm["password"];
 
-<<<<<<< HEAD:src/clases/administrador.php
-        if (!empty($_POST)) {
-       
-        $array_info = $this->obtenerInfo($_POST["nombre_usuario"]);
-
-            foreach ($array_info as $key => $value) {
-                
-                    if ($_POST["contraseña"] == $array_info[$key]["contraseña"]){
-
-                        session_start();
-                    
-                        $_SESSION["nombre_usuario"] = $_POST["nombre_usuario"];
-                        $_SESSION["instante"] = time();
-
-                        echo "<br><br>Acceso Correcto";
-
-
-                    }else{
-
-                    echo "<br><br>Contraseña incorrecta";
-
-                    }
-            }
-=======
         $infoUser = $this->obtenerInfo($user);
 
         if (!$infoUser) { // si el usuario no existe
@@ -71,7 +36,6 @@ class administrador extends conexion
         } else {
             return false;
 
->>>>>>> 9e6908283ed2f3190b1dd5c3415492948333ba94:clases/administrador.php
         }
 
     }
