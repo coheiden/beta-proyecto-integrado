240689e37d0f04bcfa9703b4738f86d768d4bc2b
Juan Cayuela <juancayuela@gmail.com>
2021-05-13 11:20:47 +0200

    actualizando login.htm mensaje de login invalido

--- public/js/login.js
+++ public/js/login.js
@@ -1,45 +1,49 @@
-
 document.addEventListener("DOMContentLoaded", main);
 
 function main() {
   addListenerForm();
 }
 
-
-
 function addListenerForm() {
-
   let formulario = document.getElementById("formulario");
-  formulario.addEventListener('submit', function (e) {
-    
+  formulario.addEventListener("submit", function (e) {
     e.preventDefault();
     enviaForm(e.currentTarget);
-
   });
 }
 
 function enviaForm(formElement) {
-    let formData = new FormData(formElement);
-    const xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-          let respuesta = JSON.parse(this.responseText);
-          showMessage(respuesta);
-      }
-    };
-    xhttp.open("POST", "login.php", true);
-    xhttp.send(formData);
-    //console.log(formData);
+  let formData = new FormData(formElement);
+  const xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      let respuesta = JSON.parse(this.responseText);
+      showMessage(respuesta);
+    }
+  };
+  xhttp.open("POST", "login.php", true);
+  xhttp.send(formData);
+  //console.log(formData);
 }
 
 function showMessage(message) {
   //console.log(message)
   //div = document.getElementById("mensaje");
   //div.innerHTML = "Formulario " + message.mensaje;
-  console.log(message);
-
-  //window.setTimeout(function () {
-  //  window.location.href = "list.html";
-  //}, 2000);
+  console.log(message.mensaje);
   
+
+if (message.mensaje == 1) {
+  window.setTimeout(function () {
+    window.location.href = "listar_eventos.html";
+    }, 1000);
+}else {
+  console.log("login invalido");
+  let lead = document.getElementsByClassName("lead");
+  console.log(lead);
+  
+  lead[0].innerHTML = "-- login invalido --";
+  lead[0].setAttribute("class", "lead text-danger");
+}
+
 }
